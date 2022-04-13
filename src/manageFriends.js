5d8375414a1571dcfb7651e7ff23485d771816a9
Michael Dally <mddally@gmail.com>
2022-04-13 11:45:44 -0400

    commit tree 5bfb470e269265ad5a1d841a2f4b8b850d37cc75

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,14 @@
 export function manageFriends(state, action) {
-  // your code here
+  const {type, payload} = action;
+
+  switch(type){
+    case 'friends/add':
+      return {friends: [...state.friends, payload]}
+    
+    case 'friends/remove':
+      return {friends: state.friends.filter(friend => friend.id !== payload)}
+
+    default:
+      return state
+  }
 }
